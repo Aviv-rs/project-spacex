59e2692095007435515d0f14e1cca0f8c0f89408
Aviv <aviv80010@gmail.com>
2022-06-22 14:44:55 +0300

    added header and lgoo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <main class="app">
+    <AppHeader />
     <RouterView />
   </main>
 </template>
 
 <script>
 import '@/assets/styles/main.scss'
+import AppHeader from './components/AppHeader.vue'
 
-export default {}
+export default {
+  components: {
+    AppHeader,
+  },
+}
 </script>
 
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="app-header">
+    <div class="logo-container">
+      <img src="../assets/imgs/logo.png" alt="" />
+      <h3 class="logo-name">SpaceX</h3>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
