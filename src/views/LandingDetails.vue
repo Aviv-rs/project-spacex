7de639233d24ec8d0295d2c702e7455aa5c03aa2
Aviv <aviv80010@gmail.com>
2022-06-22 16:06:13 +0300

    added more info and styles to landing details page

--- src/views/LandingDetails.vue
+++ src/views/LandingDetails.vue
@@ -1,15 +1,40 @@
 <template>
-  <section v-if="landing" class="landing-details main-layout">
-    <div class="landing-img-container">
-      <img :src="landingFields.imgUrl" alt="" />
+  <section v-if="landing" class="landing-details main-layout flex">
+    <div class="landing-info flex column align-center">
+      <div class="landing-img-container">
+        <img :src="landingFields.imgUrl" alt="" />
+      </div>
+
+      <div class="txt-fields-container">
+        <h1 class="name">{{ landingFields.name }}</h1>
+        <span class="status">
+          Status:
+          <span :class="landingFields.status.toLowerCase()">
+            {{ landingFields.status }}</span
+          >
+        </span>
+        <p class="summary">Short summary: {{ landingFields.details }}</p>
+        <ul class="landing-links clean-list flex">
+          <span class="read-more"> Read more: </span>
+          <li class="link">
+            <a target="_blank" :href="landingFields.wikipedia"
+              >Wikipedia page</a
+            >
+          </li>
+          <li class="link">
+            <a target="_blank" :href="landingFields.article">SpaceX article</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <h1 class="name">Name: {{ landingFields.name }}</h1>
-    <iframe
-      width="420"
-      height="315"
-      :src="`https://www.youtube.com/embed/${landingFields.youtube_id}`"
-    >
-    </iframe>
+    <div class="video-container">
+      <iframe
+        width="100%"
+        height="450"
+        :src="`https://www.youtube.com/embed/${landingFields.youtube_id}`"
+      >
+      </iframe>
+    </div>
   </section>
 </template>
 
@@ -30,14 +55,24 @@ export default {
     landingFields() {
       const {
         name,
+        details,
         links: {
           youtube_id,
+          article,
           patch: { large },
           wikipedia,
         },
         success,
       } = this.landing
-      return { name, youtube_id, imgUrl: large, wikipedia, status: success }
+      return {
+        name,
+        youtube_id,
+        imgUrl: large,
+        wikipedia,
+        status: success ? 'Success' : 'Failed',
+        details,
+        article,
+      }
     },
   },
 }
